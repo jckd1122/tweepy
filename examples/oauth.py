54bb970a846efacd48241842f7af2e69830728f4
wenqiang <wmiao1122@gmail.com>
2012-07-12 16:41:54 -0400

    update

--- examples/oauth.py
+++ examples/oauth.py
@@ -7,14 +7,14 @@ import tweepy
 
 # The consumer keys can be found on your application's Details
 # page located at https://dev.twitter.com/apps (under "OAuth settings")
-consumer_key=""
-consumer_secret=""
+consumer_key="jQ8lLnSUe9avu4haXVBg"
+consumer_secret="yLGUBdjMYPozjUHzGNUXMMCPb29T65EURLynqZYZeI"
 
 # The access tokens can be found on your applications's Details
 # page located at https://dev.twitter.com/apps (located 
 # under "Your access token")
-access_token=""
-access_token_secret=""
+access_token="225261327-6OBMmwPaBH2lD4JZgE2TRKCSZWSt36Hk529XQGAf"
+access_token_secret="sgWXPBGRMvMUbADQyNqSTdCOxGrpIbqPM5jIukTapw"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -28,4 +28,4 @@ print api.me().name
 # If the application settings are set for "Read and Write" then
 # this line should tweet out the message to your account's 
 # timeline. The "Read and Write" setting is on https://dev.twitter.com/apps
-api.update_status('Updating using OAuth authentication via Tweepy!')
+api.update_status('Updating using via Tweepy!')
